47e8161a7fef3539f8846bb75d7638f41b55bd7d
Noah Finnerman <nonothetoad@gmail.com>
2016-02-10 13:40:50 -0500

    drop users table

--- Proj3CAPgroup/config/environments/production.rb
+++ Proj3CAPgroup/config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  #we need to change this to our hostname  after deployment
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
--- Proj3CAPgroup/db/schema.rb
+++ Proj3CAPgroup/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210174029) do
+ActiveRecord::Schema.define(version: 20160210180401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,14 +33,4 @@ ActiveRecord::Schema.define(version: 20160210174029) do
 
   add_index "locations", ["user_id"], name: "index_locations_on_user_id", using: :btree
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "password"
-    t.string   "email"
-    t.string   "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "locations", "users"
 end
