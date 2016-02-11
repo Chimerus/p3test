778be4fd50d58d1fbb098eb21cbf86534eec8a7d
Jason Chan <jlc370@gmail.com>
2016-02-10 23:18:50 -0500

    test eta api

--- Proj3CAPgroup/app/controllers/users_controller.rb
+++ Proj3CAPgroup/app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
+require 'httparty'
+
 class UsersController < ApplicationController
   def index
+  end
+
+  def eta
+    # put your google maps key in your own env.
+    @key = ENV['MAPS_KEY']
+    # set the origin, pull from location?
+    @origin = 'Chicago'
+    #set the destination, pull from database.
+    @destination = "San Francisco"
+    # set the transportation method, pull from db
+    @ride = "car"
+    # have to do this httparty hackaround 
+    response = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+@origin+'&destinations='+@destination+'&mode='+@ride+'&language=en-EN&key='+@key)
+    # turns it useful
+    parsed_response = JSON.parse(response.body)  
+    # parsed_response["rows"].first["elements"].first["duration"] gets text and value
+    @time=parsed_response["rows"].first["elements"].first["duration"]["text"]
+# debugger
+    render :eta
   end
 end
--- Proj3CAPgroup/config/routes.rb
+++ Proj3CAPgroup/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
+  get '/eta' =>'users#eta'
+
   resources :users
 
   # Example of regular route:
