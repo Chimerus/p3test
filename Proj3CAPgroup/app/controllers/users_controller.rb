575bea7ab3ea0f10c891f4bf79e1c2f15659e609
Jason Chan <jlc370@gmail.com>
2016-02-10 23:48:52 -0500

    mod with directions api... using degugger like looking into the matrix

--- Proj3CAPgroup/app/controllers/users_controller.rb
+++ Proj3CAPgroup/app/controllers/users_controller.rb
@@ -12,13 +12,17 @@ class UsersController < ApplicationController
     #set the destination, pull from database.
     @destination = "San Francisco"
     # set the transportation method, pull from db
-    @ride = "car"
+    @ride = "bicycling"
     # have to do this httparty hackaround 
-    response = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+@origin+'&destinations='+@destination+'&mode='+@ride+'&language=en-EN&key='+@key)
+    # response = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+@origin+'&destinations='+@destination+'&mode='+@ride+'&language=en-EN&key='+@key) <- distance matrix
+    response = HTTParty.get('https://maps.googleapis.com/maps/api/directions/json?origin='+@origin+'&destination='+@destination+'&mode='+@ride+'&language=en-EN&key='+@key)
     # turns it useful
     parsed_response = JSON.parse(response.body)  
+    # debugger
     # parsed_response["rows"].first["elements"].first["duration"] gets text and value
-    @time=parsed_response["rows"].first["elements"].first["duration"]["text"]
+    # @time=parsed_response["rows"].first["elements"].first["duration"]["text"] <- distance matrix
+    # look at the parsed response for directions api!... its enormous!
+    @time=parsed_response["routes"].first["legs"].first["duration"]["text"]
 # debugger
     render :eta
   end
