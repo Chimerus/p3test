b408d7dc5c096894db855f593fe743a1f18eece7
Jason Chan <jlc370@gmail.com>
2016-02-11 13:43:01 -0500

    Seeds available to sow

--- Proj3CAPgroup/app/controllers/users_controller.rb
+++ Proj3CAPgroup/app/controllers/users_controller.rb
@@ -8,17 +8,18 @@ class UsersController < ApplicationController
     # put your google maps key in your own env.
     @key = ENV['MAPS_KEY']
     # set the origin, pull from location?
-    @origin = '10 E 21st St, New York, NY 10010'
+    # "40.740082199999996,-73.9897896" this format for gps coordinates, no space!
+    @origin = "40.740082199999996,-73.9897896"
     #set the destination, pull from database.
     @destination = "1834 2nd Avenue, New York, NY"
     # set the transportation method, pull from db
-    @ride = "walking"
+    @ride = "transit"
     # have to do this httparty hackaround 
     # response = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+@origin+'&destinations='+@destination+'&mode='+@ride+'&language=en-EN&key='+@key) <- distance matrix
     response = HTTParty.get('https://maps.googleapis.com/maps/api/directions/json?origin='+@origin+'&destination='+@destination+'&mode='+@ride+'&language=en-EN&key='+@key)
     # turns it useful
     parsed_response = JSON.parse(response.body)  
-    debugger
+    # debugger
     # parsed_response["rows"].first["elements"].first["duration"] gets text and value
     # @time=parsed_response["rows"].first["elements"].first["duration"]["text"] <- distance matrix
     # look at the parsed response for directions api!... its enormous!
