941dea998904415f285cf1597578c433de2fbee7
Jason Chan <jlc370@gmail.com>
2016-02-12 12:43:56 -0500

    Current Location and tweak

--- Proj3CAPgroup/app/controllers/users_controller.rb
+++ Proj3CAPgroup/app/controllers/users_controller.rb
@@ -15,36 +15,17 @@ class UsersController < ApplicationController
     end
   end
 
-  def eta
-    # put your google maps key in your own env.
-    @key = ENV['MAPS_KEY']
-    # set the origin, pull from location?
-    # "40.740082199999996,-73.9897896" this format for gps coordinates, no space!
-    @origin = "40.740082199999996,-73.9897896"
-    #set the destination, pull from database.
-    @destination = "1834 2nd Avenue, New York, NY"
-    # set the transportation method, pull from db
-    @ride = "transit"
-    # have to do this httparty hackaround 
-    # response = HTTParty.get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+@origin+'&destinations='+@destination+'&mode='+@ride+'&language=en-EN&key='+@key) <- distance matrix
-    response = HTTParty.get('https://maps.googleapis.com/maps/api/directions/json?origin='+@origin+'&destination='+@destination+'&mode='+@ride+'&language=en-EN&key='+@key)
-    # turns it useful
-    parsed_response = JSON.parse(response.body)  
-    # debugger
-    # parsed_response["rows"].first["elements"].first["duration"] gets text and value
-    # @time=parsed_response["rows"].first["elements"].first["duration"]["text"] <- distance matrix
-    # look at the parsed response for directions api!... its enormous!
-    @time=parsed_response["routes"].first["legs"].first["duration"]["text"]
-# debugger
-    render :eta
-  end
-
   def show
     @key = ENV['MAPS_KEY']
-    # set the origin, pull from sara's geolocate when its up
-    # "40.740082199999996,-73.9897896" this format for gps coordinates, no space!
-    @origin = "1834 2nd Avenue, New York, NY"
-    @locations = Location.where("user_id = '#{current_user['id']}'")
+    # get their current location - google locate
+    response = HTTParty.post('https://www.googleapis.com/geolocation/v1/geolocate?key='+@key)
+    if response.code != 200 
+      flash.now[:error] = "Error: Cannot find location"
+    else
+      parsed_response = JSON.parse(response.body)["location"]
+      @origin = parsed_response["lat"].to_s+","+parsed_response["lng"].to_s
+      @locations = Location.where("user_id = '#{current_user['id']}'")
+     end 
     render :show
   end
 private
